012cb252dab4df71c22ae24e47a2a52276697ff6
Administrator <3533643044@qq.com>
2021-02-17 09:46:03 +0800

    测试提交

---
+++ src/SGGXY/day01_基础.java
@@ -0,0 +1,22 @@
+package SGGXY;
+// 多行注释：/**/
+//单行注释：//
+/**
+*
+* 一、变量名
+* ①、字符串：String
+* ②、布尔类型：boolean
+* ③、浮点型：float
+* ④、字符：char
+*
+* 变量的使用
+* 1.Java定义变量的格式：数据类型 变量名=变量值；
+*
+*
+*
+*/
+public class day01_基础 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
