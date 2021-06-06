0147eecbf126b237f8dc44e66a794a03c5cf4fe5
linai <3533643044@qq.com>
2021-06-06 18:11:42 +0800

    cea

---
+++ src/day01/day01_基础.java
@@ -0,0 +1,31 @@
+package day01;
+// 多行注释：/**/
+//单行注释：//
+/**
+* 一、变量名
+* ①、字符串：String
+* ②、布尔类型：boolean
+* ③、浮点型：float
+* ④、字符：char
+* 配置path：可以在任何路径下都可以执行
+* 变量的使用
+* 1.Java定义变量的格式：数据类型 变量名=变量值；
+* 2.变量的声明：数据类型 变量名；
+* 3.变量的赋值：变量名=变量值；
+*/
+//public class day01_基础 {
+//    public static void main(String[] args) {
+//        System.out.println("hello world");
+//    }
+
+   public class day01_基础{
+        public static void main(String[] args) {
+           //声明变量
+           String xm="lin";
+           String sex="男";
+           String address="北京";
+           System.out.println("姓名："+xm+"\n");
+           System.out.println("性别："+sex);
+           System.out.println("家庭地址:"+address);
+        }
+    }
---
+++ src/day02/day02.java
@@ -0,0 +1,12 @@
+package day02;
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+public class day02 {
+
+}
